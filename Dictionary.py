bcd66b28b87083520a76a703c7f104e201368d81
Jalaj2508 <122887050+Jalaj2508@users.noreply.github.com>
2023-01-17 19:37:45 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+D = {1:"apple",2:35,3:"data",2:"every",6:"35.4",7:35.4}
+D
+
+
+# In[ ]:
+
+
+#Key Value Pair
+#Key is always unique
+#is mutable
+#values can be duplicated but key is unique
+
+
+# In[2]:
+
+
+D[7]
+
+
+# In[3]:
+
+
+#Add key value pair
+D[5] = 77
+D
+
+
+# In[ ]:
+
+
+
+
