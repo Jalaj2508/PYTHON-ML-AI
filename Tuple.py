d28dd1efafaa07454131ca858affca24c0e00f1c
Jalaj2508 <122887050+Jalaj2508@users.noreply.github.com>
2023-01-17 19:10:23 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+T = (1,13.6,"hi",12,1)
+
+
+# In[3]:
+
+
+print(T)
+
+
+# In[4]:
+
+
+type(T)
+
+
+# In[5]:
+
+
+### Tuple is immutable and with ()
+T[3]
+
+
+# In[6]:
+
+
+T
+
+
+# In[ ]:
+
+
+
+
