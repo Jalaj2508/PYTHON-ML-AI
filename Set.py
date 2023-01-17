02b17bc3f2c5f25e1961448abcba3ba40e4922fd
Jalaj2508 <122887050+Jalaj2508@users.noreply.github.com>
2023-01-17 19:23:35 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+S = {1,13.6,"hi",12,1,126,25,25,"apple","king","ball"}
+S
+
+
+# In[3]:
+
+
+S[4] = "6"
+
+
+# In[ ]:
+
+
+#### Does not allow duplicate
+## cannot access it using index in a set
+## Set is immutable
+# the values are in ascending order
+
+
+# In[5]:
+
+
+#removing element
+S.remove(1)
+S.add(100)
+S
+
+
+# In[ ]:
+
+
+
+
